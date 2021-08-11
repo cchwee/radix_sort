e46d81d4e4f114a191b3b6b05d7f220f1fa862a9
Cassandra Chwee <cc.cassandrachwee@gmail.com>
2021-08-11 23:02:14 +0800

    Changed ipynb file to py file, backbone code of num_rad_sort

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+# Radix sort
+def num_rad_sort(nums, b):
+    # returns a sorted list nums in asc order
+    return nums
+
+# Driver code
+nums = [43, 101, 22, 27, 5, 20, 15]
+print("Output: " + str(num_rad_sort(nums, 4)))
