055821a0ff3542fc77c993dfe042c912c3331d24
Cassandra Chwee <cc.cassandrachwee@gmail.com>
2021-08-12 23:09:43 +0800

    Edits to comments

--- assignment1.py
+++ assignment1.py
@@ -3,16 +3,14 @@ def counting_sort(a_list, base, exp):
 
     # initialise the count_array
     count_array = [[] for i in range(base+1)]
-    print("empty count arr: " + str(count_array))
+    # print("empty count arr: " + str(count_array))
 
-    # sort based on the index exp
     # formula = (number // base**exp) % base 
     # put in the item as linked list into count_array
     for num in a_list:
         digit = (num // base ** exp) % base 
         count_array[digit].append(num)
-    print("now: " + str(count_array))
-
+    # print("now: " + str(count_array))
 
     # iterate through count_array and return output
     output_lst = []
@@ -32,13 +30,13 @@ def num_rad_sort(nums, b):
     for item in nums:
         if item > max_num:
             max_num = item
-    print("max num is: " + str(max_num))
+    # print("max num is: " + str(max_num))
 
     exp = 0
     while (max_num // b**exp) > 0:
         nums = counting_sort(nums, b, exp)
         exp += 1
-        print(nums)
+        # print(nums)
 
     # returns a sorted list nums in asc order
     return nums
@@ -48,3 +46,5 @@ nums = [43, 101, 22, 27, 5, 50, 15]
 nums1 = [1,2,3,1,3,2,4]
 print("nums: " + str(nums))
 print(num_rad_sort(nums, 4))
+print("nums1: " + str(nums1))
+print(num_rad_sort(nums1, 4))
