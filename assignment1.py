eed5797ea76c1cb107a7c1cf6a6b77e9ac6d19d6
Cassandra Chwee <cc.cassandrachwee@gmail.com>
2021-08-12 23:07:55 +0800

    Radix Sort implementation using Linked List

--- assignment1.py
+++ assignment1.py
@@ -1,48 +1,50 @@
 # Counting sort
-def counting_sort(a_list):
-
-    # find max element in list
-    max_item = a_list[0]
-    for item in a_list:
-        if item > max_item:
-            max_item = item
+def counting_sort(a_list, base, exp):
 
     # initialise the count_array
-    count_array = [0] * (max_item + 1)
-    print(count_array)
+    count_array = [[] for i in range(base+1)]
+    print("empty count arr: " + str(count_array))
 
-    # iterate through the input list again
-    # put in the frequency into count_array
-    for item in a_list:
-        count_array[item] += 1
-    print(count_array)
+    # sort based on the index exp
+    # formula = (number // base**exp) % base 
+    # put in the item as linked list into count_array
+    for num in a_list:
+        digit = (num // base ** exp) % base 
+        count_array[digit].append(num)
+    print("now: " + str(count_array))
 
-    # update and sort the list
-    index = 0
-    for i in range(len(count_array)):
-        item = i
-        frequency = count_array[i]
-        for j in range(frequency):
-            a_list[index] = item
-            index += 1
+
+    # iterate through count_array and return output
+    output_lst = []
+    for lst in count_array:
+        for item in lst:
+            output_lst.append(item)
 
     # returns a sorted list
-    return a_list
-
+    return output_lst
 
 
 # Radix sort
 def num_rad_sort(nums, b):
-    # get the max digit number
     
-    # formula = (number // 10**index) % base 
+    # get the max num in nums
+    max_num = nums[0]
+    for item in nums:
+        if item > max_num:
+            max_num = item
+    print("max num is: " + str(max_num))
+
+    exp = 0
+    while (max_num // b**exp) > 0:
+        nums = counting_sort(nums, b, exp)
+        exp += 1
+        print(nums)
 
     # returns a sorted list nums in asc order
     return nums
 
 # Driver code
-nums = [43, 101, 22, 27, 5, 20, 15]
-nums1 = [1,2,3,4,1,3,2,7]
-print("nums1: " + str(nums1))
-print(counting_sort(nums1))
-print("Output: " + str(num_rad_sort(nums, 4)))
+nums = [43, 101, 22, 27, 5, 50, 15]
+nums1 = [1,2,3,1,3,2,4]
+print("nums: " + str(nums))
+print(num_rad_sort(nums, 4))
