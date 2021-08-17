aa53a6bdc138c28fc2198da90c0ba1b628208b4a
Cassandra Chwee <cc.cassandrachwee@gmail.com>
2021-08-18 01:58:38 +0800

    general commits

--- assignment1.py
+++ assignment1.py
@@ -118,7 +118,9 @@ def countsort_strings(lst: list, col: int, base: int, min_base: int) -> list:
 
 
 # radix sort from msb char --> lsb char
-def radsort_strings(lst: list, base: int, min_base: int) -> list:
+def radsort_strings(lst: list) -> list:
+
+    result_lst = []
 
     # get the max length in lst of strings
     if len(lst) > 1:
@@ -130,7 +132,7 @@ def radsort_strings(lst: list, base: int, min_base: int) -> list:
 
         # perform sort from LSB col --> MSB col
         for col in range((max_len-1), -1, -1):
-            result_lst = countsort_strings(lst, col, base, min_base)
+            result_lst = countsort_strings(lst, col, 26, ord('a'))
 
     return result_lst        
 
@@ -160,18 +162,18 @@ def optimized_countsort(lst, base, exp):
 # optimized num rad sort to sort based on length
 def optimized_num_radsort(lst):
 
-    # get max length
-    max_len = len(lst[0][1])
-    for item in lst:
-        interest_lst = item[1]
-        if len(interest_lst) > max_len:
-            max_len = len(interest_lst)
+    if len(lst) > 1:
+        # get max length 
+        max_len = len(lst[0][1])
+        for item in lst:
+            interest_lst = item[1]
+            if len(interest_lst) > max_len:
+                max_len = len(interest_lst)
 
-    exp = 0
-    while (max_len // 10**exp) > 0:
-        lst = optimized_countsort(lst, 10, exp)
-        exp += 1
-        # print(nums)
+        exp = 0
+        while (max_len // 10**exp) > 0:
+            lst = optimized_countsort(lst, 10, exp)
+            exp += 1
 
     # return output of sorted lst
     return lst
@@ -190,72 +192,76 @@ def compare_lists(lst_a: list, lst_b: list) -> bool:
 # interest group function
 def interest_groups(data) -> list:
 
-    # intialise base values
-    base = 26
-    min_base = ord('a')
-
     # do radix sort on interest list in each tuple
-    temp_lst = []
-    
-    for tuple_pair in data:
-        interest_lst = tuple_pair[1]
-        sorted_lst = radsort_strings(interest_lst, base, min_base)
-        temp_lst.append([tuple_pair[0], sorted_lst])
+    if len(data) > 1:
 
-    # sort based on length of interest list to arrange closer
-    data = optimized_num_radsort(temp_lst)
+        temp_lst = []
 
-    # check for groups
-    group_lst = []
-    pointer_1 = 0
-    pointer_2 = 1
-    
-    tmp_lst = []
+        for tuple_pair in data:
+            interest_lst = tuple_pair[1]
+            sorted_lst = radsort_strings(interest_lst)
+            temp_lst.append([tuple_pair[0], sorted_lst])
 
-    # pointer_1 first item, append name to group
-    tmp_lst.append(data[pointer_1][0])
+        # sort based on length of interest list to arrange closer
+        data = optimized_num_radsort(temp_lst)
 
-    # while loop to loop through data items
-    while pointer_1 < len(data) and pointer_2 < len(data):
+        # check for groups
+        group_lst = []
+        pointer_1 = 0
+        pointer_2 = 1
 
-        # go through data items
-        list_a = data[pointer_1][1]
-        list_b = data[pointer_2][1]
+        # pointer_1 first item, append name to group
+        tmp_lst = []
+        tmp_lst.append(data[pointer_1][0])
 
-        # if same length --> do comparison
-        if len(list_a) == len(list_b):
+        # while loop to loop through data items
+        while pointer_1 < len(data) and pointer_2 < len(data):
 
-            # if same group, append pointer_2 to tmp_lst
-            if compare_lists(list_a, list_b):
-                tmp_lst.append(data[pointer_2][0])
+            # go through data items
+            list_a = data[pointer_1][1]
+            list_b = data[pointer_2][1]
 
-                # pop the person with same interest
-                data.pop(pointer_2)
+            # if same length --> do comparison
+            if len(list_a) == len(list_b):
+
+                # if same group, append pointer_2 to tmp_lst
+                if compare_lists(list_a, list_b):
+                    tmp_lst.append(data[pointer_2][0])
+
+                    # pop the person with same interest
+                    data.pop(pointer_2)
+                
+                # if false, continue to scan through
+                else:
+                    pointer_2 += 1
             
-            # if false, continue to scan through
+            # else, definitely not in same group (diff length)
             else:
-                pointer_2 += 1
-        
-        # else, definitely not in same group (diff length)
-        else:
 
-            # append the current group to group list
-            group_lst.append(tmp_lst)
+                # append the current group to group list
+                group_lst.append(tmp_lst)
+                
+                # remove the current person 
+                data.pop(pointer_1)
+
+                # reset the pointers
+                pointer_1 = 0
+                pointer_2 = 1
+
+                # reset tmp_lst
+                tmp_lst = []
+                tmp_lst.append(data[pointer_1][0])
             
-            # remove the current person 
-            data.pop(pointer_1)
-
-            # reset the pointers
-            pointer_1 = 0
-            pointer_2 = 1
-
-            # reset tmp_lst
-            tmp_lst = []
-            tmp_lst.append(data[pointer_1][0])
-        
-    group_lst.append(tmp_lst)
+        group_lst.append(tmp_lst)
+        data = group_lst
 
     # return list of list with people of same interests
-    return group_lst
+    return data
 
+data = [("nuka", ["birds", "napping"]),
+                ("hadley", ["napping birds", "nash equilibria"]),
+                ("yaffe", ["rainy evenings", "the colour red", "birds"]),
+                ("laurie", ["napping", "birds"]),
+                ("kamalani", ["birds", "rainy evenings", "the colour red"])]
+print(interest_groups(data))
 
